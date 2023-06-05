81ae7d9f782ddaecdd8832bce3705762b5027d31
Victor Paim <victortpaim@hotmail.com>
2023-06-05 12:44:46 -0300

    commit inicial

---
+++ src/01-intro/aula01.py
@@ -0,0 +1,12 @@
+"""" aula01 """
+
+print('Olá mundo!')
+
+
+def somar(numero_01, numero_02):
+    """"somar - soma dois números"""
+    return numero_01 + numero_02
+
+
+soma = somar(10.3, 2.5)
+print(soma)
