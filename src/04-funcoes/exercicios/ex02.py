c35e1c0ccbddf181812587eee8df8f1ca224f8d5
Victor Paim <victortpaim@hotmail.com>
2023-06-14 11:25:26 -0300

    exercício 02

---
+++ src/04-funcoes/exercicios/ex02.py
@@ -0,0 +1,7 @@
+''' crie uma função que recebe três números como parâmetro (n1, n2, n3)
+e retorna a soma dos números '''
+
+def somar(n1, n2, n3):
+    return n1 + n2 + n3
+
+print(somar(7.3, 8.5, 9.7))
