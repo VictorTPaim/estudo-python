114436d846c0974459fee170b84e354f204abdf7
Victor Paim <victortpaim@hotmail.com>
2023-06-14 11:22:53 -0300

    exercício 01

---
+++ src/04-funcoes/exercicios/ex01.py
@@ -0,0 +1,8 @@
+'''crie uma função que recebe três números como parâmetro (n1, n2, n3) 
+e imprime na saída padrão a soma dos números'''
+
+def somar(n1, n2, n3):
+    soma = n1 + n2 + n3
+    print(soma)
+
+somar(7.5, 6.3, 8.8)
