e9a0170822c68ec74806ca06e2cd6a439a6940b9
Victor Paim <victortpaim@hotmail.com>
2023-06-21 13:39:25 -0300

    exercício 02

---
+++ src/08-modulos-e-pacotes/exercicios/108/moeda.py
@@ -0,0 +1,18 @@
+def metade(n):
+    return n/2
+
+
+def dobro(n):
+    return n * 2
+
+
+def aumentar(n, taxa):
+    return n * ((taxa/100) + 1)
+
+
+def diminuir(n, taxa):
+    return n * (1 - (taxa/100))
+
+
+def moeda(n, moeda='R$'):
+    return f'{moeda}{n:.2f}'.replace('.', ',')
